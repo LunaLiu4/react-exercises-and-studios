cb4b7e96aa7b5eba9dfaed9ecbc5eb2918986640
LunaLiu4 <luna.liu829@gmail.com>
2023-07-11 16:33:15 -0500

    part1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Favorite Books";
+   let book1 = "https://m.media-amazon.com/images/I/91aF1JPyq5L._SY522_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71GTmHSmomL._SY522_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/911ZFJjYM0L._SY522_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" />
+         <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" />
+         <img src={book3} alt="The London Seance Society by Sarah Penner" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,22 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from './ChoresList.module.css';
+
+// export default function ChoresList () {
+class ChoresList extends React.Component {
+   render() {
+      const chores = ['Grocery Shopping', 'Cleaning', 'Laundry', 'Dishwashing'];
+      return (
+         <div>
+            <h3 className={classes.choresHeading}>Chores List Need To Do</h3>
+            <ol>
+               <li className={classes.choresText}>{chores[0]}</li>
+               <li className={classes.choresText}>{chores[1]}</li>
+               <li className={classes.choresText}>{chores[2]}</li>
+               <li className={classes.choresText}>{chores[3]}</li>
+            </ol>
+         </div>
+      );
+   }
+
 }
+export default ChoresList;
